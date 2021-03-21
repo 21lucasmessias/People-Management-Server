ea9eed1ef96ab57845c84e9dd6b56d8ee9c7411e
21.lucasmessias <21.lucasmessias@gmail.com>
2021-03-21 19:05:34 -0300

    Conection to atlas

--- src/server.js
+++ src/server.js
@@ -3,20 +3,17 @@ const resolvers = require('./resolvers/resolvers');
 const path = require('path');
 const mongoose = require('mongoose');
 
-//using docker compose
-mongoose.connect('mongodb://127.0.0.1:27017/eprecise', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
-  if (err) {
-    console.log('Error on conection');
-    throw err;
+mongoose.connect(process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+  }, (err) => {
+    if (err) {
+      console.log('Error on conection');
+      throw err;
+    }
+    console.log("connected to mongo");
   }
-  console.log("connected to mongo");
-})
-
-//without docker compose
-/*mongoose.connect('mongodb://localhost:27017/eprecise', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
-  if (err) throw err;
-  console.log("connected to mongo");
-})*/
+)
 
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, 'schemas', 'schema.graphql'),
