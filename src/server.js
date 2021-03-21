d813cbf2f8698213f039fd54204b80fe321d01e9
21.lucasmessias <21.lucasmessias@gmail.com>
2021-03-21 17:23:16 -0300

    docker mongo config

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 //using docker compose
-/*mongoose.connect('mongodb://mongo/eprecise', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-  if (err) throw err;
-  console.log("connected to mongo");
-})*/
-
-//without docker compose
-mongoose.connect('mongodb://localhost:27017/eprecise', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
+mongoose.connect('mongodb://mongo/eprecise', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) throw err;
   console.log("connected to mongo");
 })
+
+//without docker compose
+/*mongoose.connect('mongodb://localhost:27017/eprecise', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
+  if (err) throw err;
+  console.log("connected to mongo");
+})*/
 
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, 'schemas', 'schema.graphql'),
